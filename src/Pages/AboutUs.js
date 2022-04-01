904ff062d06cf67f3c5699eec3c029903f98e503
Rahul Sharma <rs.rahulpro@gmail.com>
2022-04-01 09:23:10 +0530

    Add Services Section HTML

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,7 +1,15 @@
 import React from "react";
 import AboutSection from "../Components/AboutSection";
+import ServicesSection from "../Components/ServicesSection";
+import Faq from "../Components/Faq";
 let AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <Faq />
+    </>
+  );
 };
 
 export default AboutUs;
