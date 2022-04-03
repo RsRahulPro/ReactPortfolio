85548b63cae5da0a81eee022614d58947330e7b1
Rahul Sharma <rs.rahulpro@gmail.com>
2022-04-03 09:11:14 +0530

    Add Navigation Styling (Styled Components) without functionality

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import GlobalStyle from "./Components/GlobalStyle";
 //importing pages
 import AboutUs from "./Pages/AboutUs";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -2,6 +2,7 @@ import React from "react";
 import AboutSection from "../Components/AboutSection";
 import ServicesSection from "../Components/ServicesSection";
 import FaqSection from "../Components/FaqSection";
+import Nav from "../Components/Nav";
 let AboutUs = () => {
   return (
     <>
